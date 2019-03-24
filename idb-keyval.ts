67200aeef932e6b5e39e76f43e2f58c130b4e5af
ZaneHannanAU <ZaneHannanAU@users.noreply.github.com>
2019-03-24 23:32:42 +1100

    Attempt effective autoreload on frozen -> resume.
    Based on advice from chrome devs on the Document (page) Lifecycle API
    https://developers.google.com/web/updates/2018/07/page-lifecycle-api#advice-frozen
    
    This attempts to explicitly manage its own internal state, disconnect and reconnect after being frozen.
    
    This moves retrieval logic out of internal and into a weakmap object (a near-pure private property).
    
    A better implementation may close the attempt at creating a connection on freeze, however no such `abort` property exists on the `IDBOpenDBRequest` prototype.
    
    A minifier/tree-shaker may choose to clip the autoreload code block if it is always false, and inline the `main` arrow function directly where needed.
    
    The freeze and resume events attempt a close-early-reload-late function, where `pageshow` and `resume` are treated as independent agents performing the same function.

--- idb-keyval.ts
+++ idb-keyval.ts
@@ -1,8 +1,7 @@
+const dpb = new WeakMap<Store, Promise<IDBDatabase>>();
 export class Store {
-  readonly _dbp: Promise<IDBDatabase>;
-
-  constructor(dbName = 'keyval-store', readonly storeName = 'keyval') {
-    this._dbp = new Promise((resolve, reject) => {
+  constructor(dbName = 'keyval-store', readonly storeName = 'keyval', autoreload = false) {
+    const main = (resolve, reject) => {
       const openreq = indexedDB.open(dbName, 1);
       openreq.onerror = () => reject(openreq.error);
       openreq.onsuccess = () => resolve(openreq.result);
@@ -11,11 +10,35 @@ export class Store {
       openreq.onupgradeneeded = () => {
         openreq.result.createObjectStore(storeName);
       };
-    });
+    }
+    dbp.set(this, new Promise(main));
+    if (autoreload) {
+      // Will cause a memory leak in scripts that retrieve a new Store each time.
+      // Don't use autoreload in that case.
+      let curr: IDBDatabase | null = null;
+      let r: ((d: IDBDatabase) => void), j: ((e: any) => void);
+      dpb.get(this)!.then(db => {curr = db});
+      let frozen = 0;
+      const freeze = () => {
+        if (frozen++) return;
+        curr && curr.close();
+        curr = null;
+        dpb.set(this, new Promise((R, J) => {r = R; j = J}));
+      }
+      const resume = () => {
+        if (--frozen || !(r && j)) return;
+        dpb.get(this)!.then(db => {curr = db});
+        curr || main(r, j);
+      }
+      document.addEventListener('freeze', freeze, {capture: true});
+      window.addEventListener('pagehide', freeze, {capture: true});
+      document.addEventListener('resume', resume, {capture: true});
+      window.addEventListener('pageshow', resume, {capture: true});
+    }
   }
 
   _withIDBStore(type: IDBTransactionMode, callback: ((store: IDBObjectStore) => void)): Promise<void> {
-    return this._dbp.then(db => new Promise<void>((resolve, reject) => {
+    return dbp.get(this)!.then(db => new Promise<void>((resolve, reject) => {
       const transaction = db.transaction(this.storeName, type);
       transaction.oncomplete = () => resolve();
       transaction.onabort = transaction.onerror = () => reject(transaction.error);
